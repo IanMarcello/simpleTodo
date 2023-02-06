fca88a13eff101a36b59ea29c3564cdf50f92863
IanMarcello <ian.marcello.jada19@gmail.com>
2023-02-07 01:00:06 +0300

    Update http.js

--- src/services/http.js
+++ src/services/http.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const http = axios.create({
-  // baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
