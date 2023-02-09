9843462f214ad9646865de83fed0bb56c74333cd
IanMarcello <ian.marcello.jada19@gmail.com>
2023-02-09 07:49:27 +0300

    Create LandingView.vue
    Added new view for guest

---
+++ src/views/LandingView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import HeaderComponent from "@/components/HeaderComponent.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
+import SideNavComponent from "@/components/SideNavComponent.vue";
+</script>
+
+<template>
+  <HeaderComponent />
+  <SideNavComponent />
+  <main>
+    <div class="flex flex-col py-40">
+      <div class="text-center">
+        <h1
+          class="capitalize mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl"
+        >
+          This is the Landing Page
+        </h1>
+      </div>
+    </div>
+  </main>
+  <FooterComponent />
+</template>
