5cb0161f5223e8e19d2098e40c8e78792cdb6502
IanMarcello <ian.marcello.jada19@gmail.com>
2023-02-11 23:57:16 +0300

    Update main.js
    Added i18n for translations

--- src/main.js
+++ src/main.js
@@ -1,14 +1,24 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { createI18n } from "vue-i18n";
+import messages from "@intlify/unplugin-vue-i18n/messages";
 
 import App from "@/App.vue";
 import router from "@/router";
 
 import "@/assets/main.css";
 
+const i18n = createI18n({
+  legacy: false,
+  locale: "sw",
+  fallbackLocale: "en",
+  messages,
+});
+
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(i18n);
 app.use(router);
 
 app.mount("#app");
