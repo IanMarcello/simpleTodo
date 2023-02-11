6491ac7b20a3c1d4e4e91427788ecf8ab60e5a11
IanMarcello <ian.marcello.jada19@gmail.com>
2023-02-11 23:53:55 +0300

    Updated user/guest views
    Deleted HomeView and split it into GuestView and UserView
    Added i18n for trial on translations

---
+++ src/views/GuestView.vue
@@ -0,0 +1,9 @@
+<template>
+  <main class="flex flex-1 px-2 -mt-44 sm:-mt-0 items-center justify-center">
+    <h1
+      class="capitalize mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl"
+    >
+      THIS IS THE LANDING PAGE
+    </h1>
+  </main>
+</template>
---
+++ src/views/UserView.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { useI18n } from "vue-i18n";
+
+const user = JSON.parse(sessionStorage.getItem("user")).name;
+
+const { t } = useI18n();
+</script>
+
+<template>
+  <main
+    class="flex flex-1 -mt-c70 sm:-mt-0 text-center sm:items-center justify-center"
+  >
+    <div>
+      <p class="text-base font-semibold text-indigo-600">Welcome user,</p>
+      <h1
+        class="capitalize text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl"
+      >
+        {{ user }} {{ t("title") }}
+      </h1>
+
+      <div class="mt-6">
+        <router-link
+          :to="{ name: 'todo' }"
+          class="text-base font-medium text-indigo-600 hover:text-indigo-500"
+        >
+          See Your Tasks
+          <span aria-hidden="true"> &rarr;</span>
+        </router-link>
+      </div>
+    </div>
+  </main>
+</template>
