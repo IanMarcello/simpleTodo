08d330f753d1e90def1a63129e027f0a706f9baa
IanMarcello <ian.marcello.jada19@gmail.com>
2023-02-11 23:58:07 +0300

    Update tailwind.config.js
    Added color gold

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,25 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gold: {
+          50: "#fffdf2",
+          100: "#fffbe6",
+          200: "#fff5bf",
+          300: "#ffef99",
+          400: "#ffe34d",
+          500: "#ffd700",
+          600: "#e6c200",
+          700: "#bfa100",
+          800: "#998100",
+          900: "#7d6900",
+        },
+      },
+      spacing: {
+        c70: "16.6rem",
+      },
+    },
   },
   plugins: [
     require("@tailwindcss/line-clamp"),
